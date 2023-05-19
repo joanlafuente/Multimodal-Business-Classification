d1ea5245e056472cf5f45b63b69b79bed5b25fbe
Maria <mariaepc31@gmail.com>
2023-05-19 23:07:02 +0200

    using keras to create the annotations

--- utils/utils.py
+++ utils/utils.py
@@ -32,32 +32,14 @@ def make_loader(dataset, batch_size):
     return loader
 
 
-def make_prev(config, device="cuda"):
-    # Make the data
-    train, test = get_data(train=True), get_data(train=False)
-    train_loader = make_loader(train, batch_size=config.batch_size)
-    test_loader = make_loader(test, batch_size=config.batch_size)
-
-    # Make the model
-    model = ConTextTransformer(config.classes).to(device)
-
-    # Make the loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(
-        model.parameters(), lr=config.learning_rate)
-    
-    return model, train_loader, test_loader, criterion, optimizer
-
-
-
-
 def make(config, device="cuda"):
     # Make the data and model
-    data_path = "C:/Users/Joan/Desktop/Deep_Learning_project/features/data/"
+    data_path = "C:\Users\Maria\OneDrive - UAB\Documentos\2º de IA\NN and Deep Learning\Project\data"
     img_dir = data_path + "JPEGImages"
     txt_dir = data_path + "ImageSets/0"
-    anotation_path= "C:/Users/Joan/Desktop/Deep_Learning_project/dlnn-project_ia-group_15/anotations.pkl"
+    anotation_path= "anotations.pkl"
     input_size = 224
+    
     data_transforms_train = torchvision.transforms.Compose([
         torchvision.transforms.Resize(236, interpolation=torchvision.transforms.InterpolationMode.BICUBIC),
         torchvision.transforms.RandomResizedCrop(input_size),
@@ -65,6 +47,7 @@ def make(config, device="cuda"):
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
+    
     data_transforms_test = torchvision.transforms.Compose([
             torchvision.transforms.Resize(236, interpolation=torchvision.transforms.InterpolationMode.BICUBIC),
             torchvision.transforms.CenterCrop(input_size),
